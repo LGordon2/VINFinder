40a5704b824dd776e5133f2c07172ed2f55d81d1
lew <LGordon2@gmail.com>
2014-03-12 13:21:31 -0400

    Added a bit more to the README and moved the files around.

--- README.md
+++ README.md
@@ -3,4 +3,17 @@ VINFinder
 
 Finds VINs using eBay.
 
-Requires pip..
+Requirements:
+
+	Pip -- http://pip.readthedocs.org/en/latest/installing.html
+
+Getting started:
+
+    pip install -r requirements.txt
+    python VINFinder.py
+
+Notes:
+
+    The "boat" argument creates a predefined search on boats.
+
+    If you use the "url" argument the next argument can be a custom url to scrape from.
--- VINFinder.py
+++ VINFinder.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python
+
 #
 #  VINFinder.py
 #  
 
+from pyquery import PyQuery as pq
+from lxml import etree
 import socket, threading, re, sys, csv, urllib, urlparse, httplib, time
+
 from Queue import *
-MAX_THREADS = 50
+MAX_THREADS = 25
 DEBUG = True
 thread_pool = threading.BoundedSemaphore(value=MAX_THREADS)
 
@@ -25,6 +30,7 @@ def makeNewFinders():
 		elif not allVINFinders.empty():
 			thread_pool.acquire()
 			finder = threading.Thread(target=VINFinderFunction, args=[allVINFinders.get()])
+			if DEBUG: print "VINs left to retrieve ["+repr(allVINFinders.qsize()+1)+"]"
 			finder.start()
 
 def makeLinkRetrievers():
@@ -65,20 +71,24 @@ def LinkRetrieverFunction(url):
 
 def VINFinderFunction(url):
 	parsed_url = urlparse.urlparse(url)
-	recv_message = readMessageFromServer(parsed_url.hostname, parsed_url.path)
-	dataMatch = re.search("VIN#.*?<span>(\w+)</span>",recv_message)
-	if dataMatch is not None:
-		allFoundVINs.put(dataMatch.group(1))
+	pyqueryObj = pq(url)
+	dataMatch = ""
+	for element in pyqueryObj("td.attrLabels"):
+	  if element.text_content().strip() == "VIN (Vehicle Identification Number):" or element.text_content().strip() == "Hull ID Number:":
+	    dataMatch = element.getnext().text_content().strip()
+	    break
+	if dataMatch != "":
+		allFoundVINs.put(dataMatch)
 	thread_pool.release()
 	allVINFinders.task_done()
-	if DEBUG: print "VINs left to retrieve ["+repr(allVINFinders.qsize())+"]"
+	
 		
 #Create a queue of all the VINs we find that we need to print.
 allFoundVINs = Queue()
 allFoundVINset = set()
 allLRs = Queue()
 allVINFinders = Queue()
-filename = 'VINs.csv'
+filename = 'VINs_'+repr(int(time.time()))+'.csv'
 output = open(filename,'wb+')
 VINWriter = threading.Thread(target=outputVIN, args=[filename])
 VINWriter.daemon = True
@@ -92,17 +102,38 @@ LinkRetrieverMaker = threading.Thread(target=makeLinkRetrievers)
 LinkRetrieverMaker.daemon = True
 LinkRetrieverMaker.start()
 
-url = 'http://www.ebay.com/sch/Cars-Trucks-/6001/i.html'
+if len(sys.argv) > 1:
+	if sys.argv[1] == 'boat':
+		url = 'http://www.ebay.com/sch/Boats-/26429/i.html?_ipg=200'
+	elif sys.argv[1] == 'url':
+		if len(sys.argv) == 2:
+			print "Not enough arguments specified for this option."
+			exit(1)
+		url = sys.argv[2]
+	else:
+		url = 'http://www.ebay.com/sch/Cars-Trucks-/6001/i.html?_ipg=200'
+else:
+	url = 'http://www.ebay.com/sch/Cars-Trucks-/6001/i.html?_ipg=200'
+
+#Fix for when we are looking on multiple pages
+if "?" not in url:
+	url += "?"
+
 #Check how many pages can be searched.
 print "Connecting to first page..."
-recv_message = readMessageFromServer(urlparse.urlparse(url).hostname,url)
-matches = re.search("<span class=\"page\">Page\s*(\d+)\s*of\s*(\d+)</span>",recv_message)
-number_of_pages = 286#int(matches.group(2))
+d = pq(url=url)
+number_of_pages = int(d("span.rcnt").text().replace(",",""))/200 + 1
 
 #Look for the VINs on all the pages.
 print "Looking for VINs on ["+ repr(number_of_pages) +"] pages."
-for page_num in range(1,number_of_pages):
-	allLRs.put(url+"?_pgn="+repr(page_num))
+
+if number_of_pages == 1:
+	page_range = [1] 
+else:
+	page_range = range(1,number_of_pages)
+
+for page_num in page_range:
+	allLRs.put(url+"&_pgn="+repr(page_num))
 	
 #Wait for all tasks to be done.
 allLRs.join()
